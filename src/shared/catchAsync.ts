041655100f51aa44a7f32b6e0e9f8ea817351e28
morshed0099 <mdgolammorshed0099@gmail.com>
2024-11-09 20:06:56 +0600

    catchAsync function added

---
+++ src/shared/catchAsync.ts
@@ -0,0 +1,12 @@
+import { Request, RequestHandler, Response, NextFunction } from "express";
+
+const catchAsync = (fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+export default catchAsync
